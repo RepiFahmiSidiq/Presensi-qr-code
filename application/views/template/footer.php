ea385ed293484997e1642a9d597b5ab15263c761
iamrefijp <1906014@itg.ac.id>
2023-08-27 22:42:14 +0700

    first commit

---
+++ application/views/template/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+    </div>
+    <strong> &copy; 2022 <a href="http://localhost/sistempresensidisparbud/login">Presensi Karyawan</a>.</strong> Mahasiswa ITG | Repost by <a href='https://visitgarut.garutkab.go.id/' title='Visitdisparbud.go.id' target='_blank'>Visitdisparbud.go.id</a>
+	
+</footer>
